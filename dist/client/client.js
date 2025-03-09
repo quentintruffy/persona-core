6158545f2c9287cc500165f90dba42271c6f2195
QuentinSoon <150387351+QuentinSoon@users.noreply.github.com>
2025-03-09 20:07:20 +0100

    .

--- dist/client/client.js
+++ dist/client/client.js
@@ -1,1 +1,45 @@
 "use strict";
+on('onClientResourceStart', async (resourceName) => {
+    if (GetCurrentResourceName() !== resourceName) {
+        return;
+    }
+    console.log(`onClientResourceStart: ${resourceName}`);
+    await initializePlayer();
+});
+const spawnPos = { x: -269.4, y: -955.3, z: 31.2, heading: 205.0 };
+const initializePlayer = async () => {
+    try {
+        const modelName = 'mp_m_freemode_01';
+        const modelHash = GetHashKey(modelName);
+        if (!modelHash) {
+            throw new Error(`Model ${modelName} not found`);
+        }
+        if (!IsModelInCdimage(modelHash)) {
+            throw new Error(`Model ${modelName} not in cdimage`);
+        }
+        RequestModel(modelHash);
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        const playerId = PlayerId();
+        SetPlayerModel(playerId, modelHash);
+        await new Promise((resolve) => setTimeout(resolve, 0));
+        const ped = PlayerPedId();
+        DoScreenFadeOut(0);
+        ShutdownLoadingScreen();
+        ShutdownLoadingScreenNui();
+        await new Promise((resolve) => setTimeout(resolve, 500));
+        SetEntityCoords(ped, spawnPos.x, spawnPos.y, spawnPos.z, false, false, false, false);
+        SetEntityHeading(ped, spawnPos.heading);
+        SetEntityVisible(ped, true, false);
+        NetworkResurrectLocalPlayer(spawnPos.x, spawnPos.y, spawnPos.z, spawnPos.heading, playerId, true);
+        ClearPedTasksImmediately(PlayerPedId());
+        SetGameplayCamRelativeHeading(0);
+        FreezeEntityPosition(ped, false);
+        SetPlayerControl(playerId, false, 0);
+        DoScreenFadeIn(500);
+        SetPedDefaultComponentVariation(ped);
+        SetModelAsNoLongerNeeded(modelHash);
+    }
+    catch (error) {
+        console.error('Error initializing player:', error);
+    }
+};
