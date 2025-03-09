cf94947df88d895d664d60615a7b352c7ffb359c
QuentinSoon <150387351+QuentinSoon@users.noreply.github.com>
2025-03-09 19:50:14 +0100

    .

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,7 @@ game 'gta5'
 description 'Persona Core'
 version '1.0.0'
 
-server_script 'src/server/server.js'
-client_script 'src/client/client.js'
+server_script 'dist/server/server.js'
+client_script 'dist/client/client.js'
 
-shared_script 'src/shared/shared.js'
+shared_script 'dist/shared/shared.js'
