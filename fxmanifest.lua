4165d3ca5dd164fc2593669aca7a9754e326c08e
QuentinSoon <150387351+QuentinSoon@users.noreply.github.com>
2025-03-09 19:11:57 +0100

    intiial

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Persona Core'
+version '1.0.0'
+
+server_script 'server/server.ts'
+client_script 'client/client.ts'
+
+shared_script 'shared/shared.ts'
